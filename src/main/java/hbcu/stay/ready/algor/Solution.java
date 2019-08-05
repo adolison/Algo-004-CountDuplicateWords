2bd19a6f0f4c46ebfa0e14f46a6affe55fba80c6
adolison <amdol1@morgan.edu>
2019-08-05 09:43:31 -0400

    added words to tree set

--- src/main/java/hbcu/stay/ready/algor/Solution.java
+++ src/main/java/hbcu/stay/ready/algor/Solution.java
@@ -1,8 +1,24 @@
 package hbcu.stay.ready.algor;
 
+import java.util.Iterator;
+import java.util.TreeSet;
+
 public class Solution {
 
     public String countUniqueWords(String input){
-        return null;
+
+        //String response = "";
+        TreeSet<String> tree=new TreeSet<String>();
+        String[] parts = input.split("\\.| |s|!");
+
+        for(int i =0;i<parts.length;i++){
+            tree.add(parts[i]);
+            System.out.println(parts[i]);
+            //response += tree;
+
+        }
+        String response = ("The "+ tree.size()+" unique words are: ");
+
+        return response + tree;
     }
 }
